3cf7dfcf9f3201d2ee3f9b7330ab2bceca75247d
Mathis <mathis.schott@gmail.com>
2021-12-12 17:34:00 +0100

    add rreq and rrep converter from byte array to object off rreq / rrep

--- src/main/java/aodv/Handle/StreamDataHandle.java
+++ src/main/java/aodv/Handle/StreamDataHandle.java
@@ -1,5 +1,8 @@
 package aodv.Handle;
 
+import aodv.packages.RrepPacket;
+import aodv.packages.RreqPacket;
+
 import java.util.Base64;
 
 public class StreamDataHandle {
@@ -30,24 +33,62 @@ public class StreamDataHandle {
         return ((msg[0]) / (int) Math.pow(2, 4));
     }
 
-    public RreqPacket showRREQ(byte[] rreq){
+    /**
+     * takes a byte array that is a rreq message and turns it into an rreq object
+     * @param rreq message as a byte array
+     * @return rreq object
+     */
+    public RreqPacket returnRREQ(byte[] rreq){
+
+        RreqPacket returnRreq = new RreqPacket();
 
         int type = 0;
-        int flags;
-        if(rreq[0]%2==1){
-            flags = 1;
+        int flags = returnFlags(rreq[0]);
+
+        returnRreq.setMessageType(type);
+        returnRreq.setFlags(flags);
+        returnRreq.setHopAddress(rreq[1]);
+        returnRreq.setPrevHopAddress(rreq[2]);
+        returnRreq.setRequestId(rreq[3]);
+        returnRreq.setDestAddress(rreq[4]);
+        returnRreq.setDestSequence(rreq[5]);
+        returnRreq.setHopCount(rreq[6]);
+        returnRreq.setOriginAddress(rreq[7]);
+        returnRreq.setOriginSequence(rreq[8]);
+
+        return returnRreq;
+    }
+
+    /**
+     * takes a byte array that is a rrep message and turns it into an rrep object
+     * @param rrep message as a byte array
+     * @return rrep object
+     */
+    public RrepPacket returnRREP(byte[] rrep){
+        RrepPacket rrepPacket = new RrepPacket();
+
+        int type = 1;
+        int flags = returnFlags(rrep[0]);
+
+        rrepPacket.setMessageType(type);
+        rrepPacket.setFlags(flags);
+        rrepPacket.setHopAddress(rrep[1]);
+        rrepPacket.setPrevHopAddress(rrep[2]);
+        rrepPacket.setRequestId(rrep[3]);
+        rrepPacket.setDestAddress(rrep[4]);
+        rrepPacket.setDestSequence(rrep[5]);
+        rrepPacket.setHopCount(rrep[6]);
+        rrepPacket.setOriginAddress(rrep[7]);
+        rrepPacket.setTtl(rrep[8]);
+
+        return rrepPacket;
+    }
+
+    private int returnFlags(byte typeAndFlags){
+        if(typeAndFlags%2==1){
+            return 1;
         }
-        else{
-            flags = 0;
-        }
-        int hopAddress = byte[1];
-
-
-
-        RreqPacket rreqPacket = new RreqPacket();
-        rreqPacket.setMessageType(0);
-
-        rreqPacket.se
+        return 0;
     }
 
 }
