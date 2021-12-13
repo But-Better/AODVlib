78f1d2dd2f90bd8033adc74359d8be194f92d796
kev <kevHTW@gmx.de>
2021-12-13 11:22:26 +0100

    format

--- src/main/java/aodv/Handle/StreamData.java
+++ src/main/java/aodv/Handle/StreamData.java
@@ -2,17 +2,7 @@ package aodv.Handle;
 
 public class StreamData {
 
-    private StreamDataHandle streamDataHandle = null;
-
-    public StreamData() {
-        this.streamDataHandle = new StreamDataHandle();
-    }
-
-    public StreamData(StreamDataHandle streamDataHandle) {
-        this.streamDataHandle = streamDataHandle;
-    }
-
     public synchronized void handle(byte[] packets) {
-        this.streamDataHandle.readDataTraffic(packets);
+        StreamDataHandle.readDataTraffic(packets);
     }
 }
