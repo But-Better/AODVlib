8e11ea6fcc3518048fd7e0bfa768c690ad680ff8
kev <kevHTW@gmx.de>
2021-12-15 14:53:23 +0100

    add init methode

--- src/main/java/Init.java
+++ src/main/java/Init.java
@@ -1,8 +1,32 @@
 import aodv.Handle.Util;
+import lora.LoraCommando;
+
+import java.util.ArrayList;
 
 public class Init {
 
-    public Init(int address) {
-        Util.addToRoutingRowAsInit(address);
+    private static int address;
+    private static int addressDest;
+
+    public Init(int address, int addressDest) {
+        Init.address = address;
+        Init.addressDest = addressDest;
+        Util.addToRoutingRowAsInit(Init.address);
+    }
+
+    public ArrayList<byte[]> getAddressDestAsLoraCommand() {
+        String[] result = LoraCommando.changeAddress(String.valueOf(address));
+        ArrayList<byte[]> list = new ArrayList<>();
+        list.add(result[0].getBytes());
+        list.add(result[1].getBytes());
+        return list;
+    }
+
+    public static int getAddressDest() {
+        return addressDest;
+    }
+
+    public static int getAddress() {
+        return address;
     }
 }
